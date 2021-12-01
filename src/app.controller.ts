92c458c1f308a6879fb9e6bbc436d51f1f28a811
pierdolic <kykyryza2027@gmail.com>
2021-12-01 19:49:07 +0200

    lab3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): number {
-  return this.appService.getNumbers();
+  getHello(): Promise<any>  {
+  return this.appService.getHello  ();
   }
 }
